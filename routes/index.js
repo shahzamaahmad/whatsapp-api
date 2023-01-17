42c157786d6ab3e8eca0d1bbebeafef9421c1cda
shahzamaahmad <dev.shahzama@gmail.com>
2023-01-17 18:01:38 +0530

    edit message response

--- routes/index.js
+++ routes/index.js
@@ -24,7 +24,7 @@ router.post('/webhook', async function (req, res, next) {
       let phone_number_id = req.body.entry[0].changes[0].value.metadata.phone_number_id;
       let from = req.body.entry[0].changes[0].value.messages[0].from; // extract the phone number from the webhook payload
       let msg_body = req.body.entry[0].changes[0].value.messages[0].text.body; // extract the message text from the webhook payload
-      let username = req.body.entry[0].changes[0].value.contact[0].profile.name
+      // let username = req.body.entry[0].changes[0].value.contact[0].profile.name
       axios({
         method: "POST", // Required, HTTP method, a string, e.g. POST, GET
         url:
@@ -35,7 +35,7 @@ router.post('/webhook', async function (req, res, next) {
         data: {
           messaging_product: "whatsapp",
           to: from,
-          text: { body: "Ack: " + username + msg_body },
+          text: { body: "Ack: " + msg_body },
         },
         headers: { "Content-Type": "application/json" },
       });
